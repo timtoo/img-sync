2ad3b5a5a1e59566b1fe9e16a81e8a9eff77301e
Tim <x@vaults.ca>
2011-09-10 10:53:55 -0400

    getting organized

--- imgsync/album.py
+++ imgsync/album.py
@@ -1,6 +1,7 @@
 
 class Album(object):
     """Album base class"""
+    service = ''
     title = None
     description = None
     date = None
@@ -12,13 +13,22 @@ class Album(object):
     def __init__(self, id):
         self.id = id
 
-
     def getPhotos(self):
+        raise RuntimeError, "not implemented"
 
     def getAlbumInfo(self):
-
+        raise RuntimeError, "not implemented"
 
     def getAlbum(self):
         """Load album info and list of photo objects"""
         self.getAlbumInfo()
         self.getPhotos()
+
+    def dump(self, f):
+        """
+        """
+
+    def load(self, f):
+        """
+        """
+
