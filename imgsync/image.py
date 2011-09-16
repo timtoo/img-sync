8f1f3b9628de11b48d6a2770ff358aa233780f69
Tim <x@vaults.ca>
2011-09-16 16:29:47 -0400

    flesh out the Image/LocalImage methods

--- imgsync/image.py
+++ imgsync/image.py
@@ -1,6 +1,12 @@
 import hashlib, json
 
 class Image(object):
+    """Base object for represeting single images.
+
+    All of the set* methods must be overridden, to provide functionality.
+    If a particular set* method is not needed, it still must be overridden
+    to avoid an exception (to ensure it explicitely is not needed).
+    """
     def __init__(self, id, filename=None, timestamp=None, title=None, description=None):
         self.service = {}
         self.id = id
@@ -15,10 +21,10 @@ class Image(object):
         self._hashFile = None
 
     @property
-    def hashFile(self):
+    def fileHash(self):
         if self._hashFile is None:
             _hash = hashlib.sha256()
-            f = open(self.id, 'rb')
+            f = self.openRawFile()
             while True:
                 data = f.read(1024000)
                 if not data:
@@ -28,8 +34,8 @@ class Image(object):
         return self._hashFile
 
     @property
-    def hashMeta(self):
-        """This includes:
+    def metaHash(self):
+        """This, used to detect a meta data change, includes:
                 - title
                 - description
                 - comments
@@ -44,30 +50,50 @@ class Image(object):
             self._hashMeta = _hash.hexdigest()
         return self._hashMeta
 
-    def getRawImage(self):
-        raise RuntimeError, "getRawImage not implemented"
+    def openFile(self):
+        raise RuntimeError, "openFile not implemented"
 
-    def getTags(self):
-        return []
+    def setDetails(self):
+        """Set object attribute details such as title, size, date, etc.
+        This method is always called before any other set* methods, so
+        it can set up object attributes to be used by other set* methods.
+        """
+        raise RuntimeError, "setDetails not implemented"
 
-    def getDescription(self):
-        return ''
+    def setTags(self):
+        """Load tags associated with object"""
+        raise RuntimeError, "setTags not implemented"
 
-    def getComments(self):
-        return []
+    def setDescription(self):
+        """Get any description applied to the object"""
+        raise RuntimeError, "setDescription not implemented"
 
-    def getGeocoding(self):
-        return
+    def setComments(self):
+        """Get any description attached to the object"""
+        raise RuntimeError, "setComments not implemented"
+
+    def setGeolocation(self):
+        """Get any description associated with the object"""
+        raise RuntimeError, "setGeolocation not implemented"
 
     def loadConfig(self, config, section):
+        """ load object from config file"""
         pass
 
+    def setMeta(self):
+        self.setDetails()
+        self.setTags()
+        self.setDescription()
+        self.setGeolocation()
+        self.setComments()
+        return self
+
     def dumpConfig(self, config, section):
+        """Dump object data to a config file"""
         config.set(section, 'id', self.id)
         self.filename and config.set(section, 'filename', self.filename or '')
         self.description and config.set(section, 'description',
                 self.description or '')
         self.timestamp and config.set(section, 'timestamp', self.timestamp)
-        print self.tags
         config.set(section, 'tags', json.dumps(self.tags))
 
