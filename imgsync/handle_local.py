bf731923a7da2113a675debc99fb0438817fd3dc
Tim Middleton <tim@vaults.ca>
2011-09-16 16:29:47 -0400

    flesh out the Image/LocalImage methods

--- imgsync/handle_local.py
+++ imgsync/handle_local.py
@@ -1,4 +1,4 @@
-"""Handle local albums/images"""
+"""Code for handling local albums/images"""
 
 import os, time, datetime, re
 from album import Album, AlbumAdaptor
@@ -9,6 +9,55 @@ try:
 except ImportError:
     print "Warning: pyexiv2 not found: embedded local image metadata will be ignored"
     pyexiv2 = None
+
+class LocalImage(Image):
+    @staticmethod
+    def stat2datetime(st):
+        """Given an os.stat structure, return datetime object"""
+        return datetime.datetime(*(time.localtime(st[8]))[0:6])
+
+    def openFile(self):
+        return open(self.id, 'rb')
+
+    def setDetails(self):
+        st = os.stat(self.id)
+        self.timestamp = self.stat2datetime(st)
+        self.title = self.filename
+
+    def setTags(self):
+        self.tags = []
+        meta = self.getExiv2()
+        if meta:
+            for k in ('Iptc.Application2.Keywords',
+                        'Xmp.dc.subject',
+                        'Xmp.digiKam.TagsList',
+                        'Xmp.MicrosoftPhoto.LastKeywordXMP',
+                        'Xmp.lr.hierarchicalSubject'):
+                if k in meta.iptc_keys and meta[k].value:
+                    self.tags = meta[k].value
+                    break
+
+    def setDescription(self):
+        meta = self.getExiv2()
+        if meta:
+            self.description = meta.comment
+        else:
+            self.description =  ''
+
+    def setGeolocation(self):
+        pass
+
+    def setComments(self):
+        pass
+
+    def getExiv2(self):
+        """Return exiv2 metadata object"""
+        if not hasattr(self, '_exiv2'):
+            if pyexiv2:
+                self._exiv2 = pyexiv2.ImageMetadata(self.id)
+                self._exiv2.read()
+        return self._exiv2
+
 
 class LocalAlbum(AlbumAdaptor):
     """Adapt Album with support for local directory functionality. Album ID is the full path."""
@@ -22,17 +71,12 @@ class LocalAlbum(AlbumAdaptor):
         # remove trailing slash from directory name
         super(LocalAlbum, self).__init__(id.rstrip(os.sep))
 
-    @staticmethod
-    def stat2datetime(st):
-        """Given an os.stat structure, return datetime object"""
-        return datetime.datetime(*(time.localtime(st[8]))[0:6])
-
     def getAlbumInfo(self):
         path = self.id
         if os.path.exists(path):
             self.url = 'file://' + path
             # directory modification time
-            self.date = self.stat2datetime(os.stat(self.id))
+            self.date = LocalImage.stat2datetime(os.stat(self.id))
             self.description = ''
             # last element in path, with underscores converted to spaces
             self.title = os.path.split(path)[-1].strip(os.sep).replace('_', ' ')
@@ -46,23 +90,9 @@ class LocalAlbum(AlbumAdaptor):
             # recognize image files by extension
             if self.img_regex.search(fn):
                 path = self.id + os.sep + fn
-                st = os.stat(path)
-                date = self.stat2datetime(os.stat(path))
-                i = Image(path, filename=fn, timestamp=date)
-                if pyexiv2:
-                    meta = pyexiv2.ImageMetadata(path)
-                    meta.read()
-                    for k in ('Iptc.Application2.Keywords',
-                                'Xmp.dc.subject',
-                                'Xmp.digiKam.TagsList',
-                                'Xmp.MicrosoftPhoto.LastKeywordXMP',
-                                'Xmp.lr.hierarchicalSubject'):
-                        if k in meta.iptc_keys and meta[k].value:
-                            i.tags = meta[k].value
-                            break
-
+                i = LocalImage(path, filename=fn)
+                i.setMeta()
                 self.images.append(i)
-
 
             # XXX todo
             # - get comment from jpeg comment
